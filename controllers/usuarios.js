518f475fa09bb22818728657da31cd2e1cc8d7e6
YazzerJM <yasserjimenezmartinez@hotmail.com>
2021-08-12 03:14:32 -0500

    Backend basico

---
+++ controllers/usuarios.js
@@ -0,0 +1,59 @@
+const { response, request } = require('express');
+
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre, apikey }= req.query;
+
+    res.json({
+        msg: 'get API - controlador',
+        q, nombre, apikey
+    });
+}
+
+const usuariosPut = (req, res) => {
+
+    const id = req.params.id;
+
+    res.json({
+        msg: "put API - controlador",
+        id
+    });
+}
+
+const usuariosPost = (req, res) => {
+    
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: "post API / controlador",
+        nombre, edad
+    });
+    
+}
+
+const usuariosPatch = (req, res) => {
+    res.json({
+        ok: true,
+        msg: "patch API - controlador"
+    });
+}
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        ok: true,
+        msg: "delete API - controlador"
+    });
+}
+
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosPatch,
+    usuariosDelete
+}
