0b970522b5076b2f0d17d8d0dcb96ed5e10726cd
YazzerJM <yasserjimenezmartinez@hotmail.com>
2021-08-19 05:56:53 -0500

    Google Signin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require("express");
+const { response, request } = require("express");
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -52,6 +53,50 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ){
+            const data = {
+                nombre, 
+                img, 
+                correo,
+                password: ':P',
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if( !usuario.estado ){
+            return res.status(401).json({
+                msg: 'Hable con el administrador - usuario bloqueado'
+            });
+        }
+
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        });
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
